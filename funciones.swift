db3b00ad9ee947bca954c8ced447f9a77768aae8
westcott99 <maurycastro4@gmail.com>
2020-07-31 04:13:12 +0000

    Actualizado

--- funciones.swift
+++ funciones.swift
@@ -1,14 +1,43 @@
-func operacionesAritmeticas(numero1 a:Double, numero2: b:Double, operador s:String){
-  switch s{
-    case "+":
-            print(a+b)
-    case "-":
-            print(a-b)
-    case "*"
-            print(a*b)
-    case "/"
-            print(a/b)
-    default:
-            print("operacion no soportada")
-  }
-}
+//func operacionesAritmeticas(numero1 a:Double,numero2 //b:Double, operador s:String){
+//  switch s {
+//  case "+":
+//            print(a+b)
+//    case "-":
+//            print(a-b)
+//    case "*":
+//            print(a*b)
+//    case "/":
+//            print(a/b)
+//    default:
+//            print("operacion no soportada")
+//  }
+//}
+//...........................
+//func pago (sueldo:Double, extras:Int, pagoExtras:Double) -> Double{
+//  let sueldoExtras = Double(extras) * pagoExtras;
+//  let sueldoTotal = sueldo + sueldoExtras
+//  let isss = sueldoTotal * 0.03
+//  let afp = sueldoTotal * 0.0625
+//  let renta = sueldoTotal * 0.1
+//  let descuentos = isss + afp + renta
+//  let totalPagar = sueldoTotal - descuentos
+//  return totalPagar
+//}
+//...........................
+//func edad(_ edad:Int) -> String{
+//  var mensaje:String
+//  if(edad < 0){
+//    mensaje = "Edad incorrecta"
+//  }
+//  else if(edad < 12){
+//    mensaje = "Menor de edad"
+//  }
+//  else if(edad >= 12 && edad < 18){
+//    mensaje = "Adolescente"
+//  }
+//  else{
+//    mensaje = "Mayor de edad"
+//  }
+//  return mensaje
+//}
+//...........................
--- main.swift
+++ main.swift
@@ -1,2 +1,90 @@
-print(operacionesAritmeticas(numero1: 2, numero2: 2, operador: "+"))
+//let num1 = 10
+//let num2 = -3
 
+//var sum = num1 + num2
+//print(sum)
+//...........................
+//for i in 1...10{
+//  print(i)
+//}
+//..........................
+//let num = 13
+//var primo = true
+
+//for i in 2...(num-1){
+//  if num/i == 0{
+//    primo = false
+//    break
+//  }
+//}
+//if(primo){
+//  print("El numero es primo")
+//}else{
+//  print("El numero no es primo")
+//}
+//..............................
+//var paises = ["El Salvador","Guatemala","Honduras","Costa Rica","Nicaragua","Belice"]
+
+//for i in 1...paises.count-1{
+  //print(paises[i])
+//}
+//print("Otra forma de iterar")
+//for item in paises{
+//  print(item)
+//}
+//................................
+//print(operacionesAritmeticas(numero1: 2, numero2: 2, operador: "+"))
+//................................
+//var sueldo:Double?, extras:Int?, pagoExtras:Double?
+//print("Ingrese sueldo")
+//let stringSueldo = readLine()
+//sueldo = Double(stringSueldo!)
+
+//print("Ingrese horas extras")
+//let stringExtras = readLine()
+//extras = Int(stringExtras!)
+
+//print("Ingrese pago de horas extras")
+//let stringPagoExtras = readLine()
+//pagoExtras = Double(stringPagoExtras!)
+
+//let resultado = pago(sueldo: sueldo!, extras: extras!, pagoExtras: pagoExtras!)
+
+//print("El total a pagar es: $\(resultado)")
+//.................................
+//print("Ingresa tu edad")
+//let miEdad = readLine()
+
+//if let numero = Int(miEdad!){
+//  print(edad(numero))
+//}else{
+//  print("No puedo procesar la solicitud")
+//}
+//.................................
+var numeros = [Int]()
+var continuar = true;
+while continuar{
+  print("Ingrese un numero")
+  let tmp = readLine()
+  if let numero = Int(tmp!){
+    numeros.append(numero)
+  }else{
+    print("No se puede agregar el numero")
+  }
+  print("¿continuar? y/n")
+  continuar = (readLine()=="y") ? true : false
+}
+
+var mayor = numeros[0]
+var menor = numeros[0]
+for numero in numeros{
+  if(numero > mayor){
+    mayor = numero
+  }
+  if(numero < menor){
+    menor = numero
+  }
+  print(numero)
+}
+print("El numero mayor es: \(mayor)")
+print("El numero menor es: \(menor)")
